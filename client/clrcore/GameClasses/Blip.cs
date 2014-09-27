9f36b52c29cf49524f7c0f5fe8ae4e0e543b61dc
Imperative <admin@impy.me>
2014-09-27 15:19:53 +0200

    Added Model class (by TheDeadlyDutchi), also added Pickup class (not finished yet)

--- client/clrcore/GameClasses/Blip.cs
+++ client/clrcore/GameClasses/Blip.cs
@@ -8,6 +8,8 @@ namespace CitizenFX.Core
 {
     public sealed class Blip : HandleObject
     {
+        private bool m_hasExisted;
+
         public float Alpha
         {
             set
@@ -84,11 +86,6 @@ namespace CitizenFX.Core
             }
         }
 
-        /*public static Blip AddBlip(Pickup pickup)
-        {
-
-        }*/
-
         public bool Friendly
         {
             set
@@ -121,12 +118,40 @@ namespace CitizenFX.Core
             }
         }
 
+        public bool Exists
+        {
+            get
+            {
+                if (m_hasExisted)
+                {
+                    return true;
+                }
+
+                if (m_handle == 0)
+                {
+                    return false;
+                }
+
+                try
+                {
+                    m_hasExisted = Function.Call<bool>(Natives.DOES_BLIP_EXIST, m_handle);
+
+                    return m_hasExisted;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
         internal override void SetHandle(int handle)
         {
             if (m_handle != -1)
                 ObjectCache<Blip>.Remove(this);
 
             m_handle = handle;
+            m_hasExisted = false;
 
             ObjectCache<Blip>.Add(this);
         }
@@ -135,14 +160,6 @@ namespace CitizenFX.Core
         {
             Display = BlipDisplay.Hidden;
             Function.Call(Natives.REMOVE_BLIP, m_handle);
-        }
-
-        public bool InternalCheckExists()
-        {
-            if (m_handle == 0)
-                return false;
-
-            return Function.Call<bool>(Natives.DOES_BLIP_EXIST, m_handle);
         }
 
         public Entity GetAttachedItem()
@@ -204,143 +221,14 @@ namespace CitizenFX.Core
 
             return ObjectCache<Blip>.Get((int)blipPtr);
         }
-    }
 
-    public enum BlipColor
-    {
-        White = 0,
-        DarkRed = 1,
-        DarkGreen = 2,
-        Cyan = 3,
-        Grey = 4,
-        Yellow = 5,
-        Orange = 6,
-        Purple = 7,
-        Green = 8,
-        Red = 9,
-        LightRed = 10,
-        LightOrange = 11,
-        DarkTurquoise = 12,
-        Turquoise = 13,
-        LightYellow = 14
-    }
+        public static Blip AddBlip(Pickup pickup)
+        {
+            Pointer blipPtr = typeof(int);
 
-    public enum BlipDisplay
-    {
-        Hidden = 0,
-        ArrowOnly = 1,
-        MapOnly = 2,
-        ArrowAndMap = 4,
-    }
+            Function.Call(Natives.ADD_BLIP_FOR_PICKUP, pickup.Handle, blipPtr);
 
-    public enum BlipIcon
-    {
-        Misc_Destination,
-        Misc_Destination1,
-        Misc_Destination2,
-        Misc_Objective,
-        Misc_Objective4,
-        Misc_Objective5,
-        Misc_Player,
-        Misc_North,
-        Misc_Waypoint,
-        Weapon_Pistol,
-        Weapon_Shotgun,
-        Weapon_SMG,
-        Weapon_Rifle,
-        Weapon_Rocket,
-        Weapon_Grenade,
-        Weapon_Molotov,
-        Weapon_Sniper,
-        Weapon_BaseballBat,
-        Weapon_Knife,
-        Pickup_Health,
-        Pickup_Armor,
-        Building_BurgerShot,
-        Building_CluckinBell,
-        Person_Vlad,
-        Building_Internet,
-        Person_Manny,
-        Person_LittleJacob,
-        Person_Roman,
-        Person_Faustin,
-        Building_Safehouse,
-        Misc_TaxiRank,
-        Person_Bernie,
-        Person_Brucie,
-        Person_Unknown,
-        Person_Dwayne,
-        Person_Elizabeta,
-        Person_Gambetti,
-        Person_JimmyPegorino,
-        Person_Derrick,
-        Person_Francis,
-        Person_Gerry,
-        Person_Katie,
-        Person_Packie,
-        Person_PhilBell,
-        Person_PlayboyX,
-        Person_RayBoccino,
-        Misc_8BALL,
-        Activity_Bar,
-        Activity_BoatTour,
-        Activity_Bowling,
-        Building_ClothShop,
-        Activity_Club,
-        Activity_Darts,
-        Person_Dwayne_Red,
-        Activity_Date,
-        Person_PlayboyX_Red,
-        Activity_HeliTour,
-        Activity_Restaurant,
-        Building_TrainStation,
-        Building_WeaponShop,
-        Building_PoliceStation,
-        Building_FireStation,
-        Building_Hospital,
-        Person_Male,
-        Person_Female,
-        Misc_FinishLine,
-        Activity_StripClub,
-        Misc_ConsoleGame,
-        Misc_CopCar,
-        Person_Dimitri,
-        Activity_ComedyClub,
-        Activity_CabaretClub,
-        Misc_Ransom,
-        Misc_CopHeli,
-        Person_Michelle,
-        Building_PayNSpray,
-        Person_Assassin,
-        Misc_Revenge,
-        Misc_Deal,
-        Building_Garage,
-        Person_Lawyer,
-        Misc_Trophy,
-        Misc_MultiplayerTutorial,
-        Building_TrainStation3,
-        Building_TrainStation8,
-        Building_TrainStationA,
-        Building_TrainStationB,
-        Building_TrainStationC,
-        Building_TrainStationE,
-        Building_TrainStationJ,
-        Building_TrainStationK,
-        Building_CarWash,
-        Person_UnitedLibertyPaper,
-        Misc_Boss,
-        Misc_Base
-    }
-
-    public enum BlipType
-    {
-        Vehicle = 1,
-        Ped = 2,
-        Object = 3,
-        Coordinate = 4,
-        Contact = 5,
-        Pickup = 6,
-        Unknown = 7,
-        Pickup2 = 8,
+            return ObjectCache<Blip>.Get((int)blipPtr);
+        }
     }
 }
